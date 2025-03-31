b26392b513144bb32ad2199ee787b1d794b3d9a9
Aman Sahu <96387100+Nikk5613@users.noreply.github.com>
2025-03-31 10:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.51%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int c  ;
-        unordered_map<int , int> mp;
-        for(int i = 0 ; i < nums.size() ; i++) {
-            mp[nums[i]]++;
+       int l = 0 ;
+       int r  = nums.size() -1 ;
+       while( l <r) {
+        int mid =  l + (r-l)/2 ;
+        if(mid%2!= 0) {
+            mid--;
         }
-        for(auto x : mp) {
-            if(x.second == 1 ){
-                c = x.first;
-            }
-        
-    }
-    return c ;
+        if(nums[mid] !=  nums[mid+1]){
+            r = mid ;
+        }
+        else {
+            l =  mid +2 ;
+        }
+       }
+       return nums[l];
     }
 };
